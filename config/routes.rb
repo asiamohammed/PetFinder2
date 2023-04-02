9e5670680f3d0990d20a58f2ffce57fbcf7901b0
asiamohammed <mohamedasia021@gmail.com>
2023-04-02 16:10:20 +0300

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+ 
+  
   resources :pets, only:[:index, :show, :create, :update, :destroy]
+  resources :users, only:[:index, :show, :create, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_074200) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_02_123020) do
   create_table "pets", force: :cascade do |t|
     t.string "image"
     t.string "name"
@@ -22,6 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_074200) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "animal_type"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
