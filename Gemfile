d2c7762838f6a85ae60ed6cfc581fffd30298dff
asiamohammed <mohamedasia021@gmail.com>
2023-04-02 16:19:03 +0300

    commit

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem "puma", "~> 5.0"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
-
+gem 'bcrypt', '~> 3.1.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_02_123020) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_02_131836) do
   create_table "pets", force: :cascade do |t|
     t.string "image"
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_02_123020) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
