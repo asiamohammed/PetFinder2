0f8a2399e89de0d740875655cc41446aa07b7241
asiamohammed <mohamedasia021@gmail.com>
2023-04-02 16:34:22 +0300

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /users
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+      # has_many :pets
+    has_secure_password
+    validates :username, presence: true
+    validates :email, presence: true, uniqueness: true
 end
